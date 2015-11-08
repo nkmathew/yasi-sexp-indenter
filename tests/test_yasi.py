ddda7c1b09426ceb74d8f1e599fdd824b574c123
nkmathew <kipkoechmathew@gmail.com>
2015-11-08 23:05:00 +0300

    Add tests for newline detection

--- tests/test_yasi.py
+++ tests/test_yasi.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-"""Test suite for yasi
+""" Test suite for yasi
 """
 
 import os
@@ -14,8 +14,32 @@ if sys.version_info < (2, 7):
 else:
     import unittest
 
+PROJECT_DIR = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]
+sys.path.insert(0, PROJECT_DIR)
+
+import yasi
+
 class UnitTests(unittest.TestCase):
-    pass
+
+    def test_find_lf_line_ending(self):
+        source = 'silent realm\n the lost dimension\n'
+        self.assertEqual(yasi.LF, yasi.line_ending(source))
+
+    def test_find_crlf_line_ending(self):
+        source = 'silent realm\r\n the lost dimension\r\n'
+        self.assertEqual(yasi.CRLF, yasi.line_ending(source))
+
+    def test_find_cr_line_ending(self):
+        source = 'silent realm\r the lost dimension\r'
+        self.assertEqual(yasi.CR, yasi.line_ending(source))
+
+    def test_find_cr_wth_lf_mixed_line_ending(self):
+        source = 'silent realm\r the lost dimension\n the lonely angel\n end \n\r'
+        self.assertEqual(yasi.CR, yasi.line_ending(source))
+
 
 class SystemTests(unittest.TestCase):
     pass
+
+if __name__ == '__main__':
+    unittest.main()
