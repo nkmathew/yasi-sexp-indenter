0741b8cabbec0358a21d9d3c72e73c4dc32dace3
nkmathew <kipkoechmathew@gmail.com>
2015-11-15 10:25:02 +0300

    Add more tests for mixed line endings

--- tests/test_yasi.py
+++ tests/test_yasi.py
@@ -17,22 +17,37 @@ import yasi
 
 class UnitTests(unittest.TestCase):
 
-    def test_find_lf_line_ending(self):
-        source = 'silent realm\n the lost dimension\n'
-        self.assertEqual(yasi.LF, yasi.line_ending(source))
+    def test_find_line_ending_only_lf(self):
+        source = 'First Line\n Second Line\n'
+        self.assertEqual(yasi.LF, yasi.find_line_ending(source))
 
-    def test_find_crlf_line_ending(self):
-        source = 'silent realm\r\n the lost dimension\r\n'
-        self.assertEqual(yasi.CRLF, yasi.line_ending(source))
+    def test_find_line_ending_only_crlf(self):
+        source = 'First Line\r\n Second Line\r\n'
+        self.assertEqual(yasi.CRLF, yasi.find_line_ending(source))
 
-    def test_find_cr_line_ending(self):
-        source = 'silent realm\r the lost dimension\r'
-        self.assertEqual(yasi.CR, yasi.line_ending(source))
+    def test_find_line_ending_only_cr(self):
+        source = 'First Line\r Second Line\r'
+        self.assertEqual(yasi.CR, yasi.find_line_ending(source))
 
-    def test_find_cr_wth_lf_mixed_line_ending(self):
-        source = 'silent realm\r the lost dimension\n the lonely angel\n end \n\r'
-        self.assertEqual(yasi.CR, yasi.line_ending(source))
+    def test_find_line_ending_lf_with_cr_and_crlf(self):
+        source = 'First Line\r Second Line\r\n Third Line\n Fourth Line \r\n'
+        self.assertEqual(yasi.CRLF, yasi.find_line_ending(source))
 
+    def test_find_line_ending_lf_with_crlf(self):
+        source = 'First Line\n Second Line\r\n Third Line\n Fourth Line \r\n'
+        self.assertEqual(yasi.CRLF, yasi.find_line_ending(source))
+
+    def test_find_line_ending_cr_with_crlf(self):
+        source = 'First Line\r Second Line\r\n Third Line\r Fourth Line \r\n'
+        self.assertEqual(yasi.CRLF, yasi.find_line_ending(source))
+
+    def test_find_line_ending_cr_with_lf(self):
+        source = 'First Line\r Second Line\n Third Line\n Fourth Line \n\r'
+        self.assertEqual(yasi.CR, yasi.find_line_ending(source))
+
+    def test_find_line_ending_should_default_to_lf(self):
+        source = 'Line without ending'
+        self.assertEqual(yasi.LF, yasi.find_line_ending(source))
 
 class SystemTests(unittest.TestCase):
     pass
